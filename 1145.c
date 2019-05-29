13603eb8e45b71818b6cab13955351222b4e7062
Akylles <akylles.brasil@ccc.ufcg.edu.br>
2019-05-29 20:00:40 -0300

    Adicionando a especificação do problema como comentário

--- 1145.c
+++ 1145.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
  
+/*
+Escreva um programa que leia dois valores X e Y. 
+A seguir, mostre uma sequência de 1 até Y, passando para a próxima linha a cada X números.
+*/
+
 int main() {
  
     int x, y, i = 0;
